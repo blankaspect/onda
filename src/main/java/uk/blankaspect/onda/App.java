a20c45734ae221dffbf93710d9e72b1c27ca93e8
Andy Morgan-Richards <id883436@prng.uk>
2017-12-03 18:40:23 +0000

    Added the 'versionStr' instance field and changed the 'getVersionString' method.

--- src/main/java/uk/blankaspect/onda/App.java
+++ src/main/java/uk/blankaspect/onda/App.java
@@ -43,10 +43,10 @@ import uk.blankaspect.common.iff.ChunkFilter;
 
 import uk.blankaspect.common.misc.ArraySet;
 import uk.blankaspect.common.misc.CalendarTime;
+import uk.blankaspect.common.misc.ClassUtils;
 import uk.blankaspect.common.misc.CommandLine;
 import uk.blankaspect.common.misc.DirectoryFilter;
 import uk.blankaspect.common.misc.FilenameFilter;
-import uk.blankaspect.common.misc.NoYes;
 import uk.blankaspect.common.misc.PropertyString;
 import uk.blankaspect.common.misc.ResourceProperties;
 import uk.blankaspect.common.misc.StringUtils;
@@ -579,40 +579,50 @@ public class App
 
 	//------------------------------------------------------------------
 
+	/**
+	 * Returns a string representation of the version of this application.  If this class was loaded from a JAR, the
+	 * string is created from the values of properties that are defined in a resource named 'build.properties';
+	 * otherwise, the string is created from the date and time when this method is first called.
+	 *
+	 * @return a string representation of the version of this application.
+	 */
+
 	public String getVersionString()
 	{
-		StringBuilder buffer = new StringBuilder(32);
-		String str = buildProperties.get(VERSION_PROPERTY_KEY);
-		if (str != null)
-			buffer.append(str);
-
-		str = buildProperties.get(RELEASE_PROPERTY_KEY);
-		if (str == null)
+		if (versionStr == null)
 		{
-			long time = System.currentTimeMillis();
-			if (buffer.length() > 0)
-				buffer.append(' ');
-			buffer.append('b');
-			buffer.append(CalendarTime.dateToString(time));
-			buffer.append('-');
-			buffer.append(CalendarTime.hoursMinsToString(time));
-		}
-		else
-		{
-			NoYes release = NoYes.forKey(str);
-			if ((release == null) || !release.toBoolean())
+			StringBuilder buffer = new StringBuilder(32);
+			if (ClassUtils.isFromJar(getClass()))
 			{
-				str = buildProperties.get(BUILD_PROPERTY_KEY);
+				// Append version number
+				String str = buildProperties.get(VERSION_PROPERTY_KEY);
 				if (str != null)
-				{
-					if (buffer.length() > 0)
-						buffer.append(' ');
 					buffer.append(str);
+
+				// If this is not a release, append build
+				boolean release = Boolean.parseBoolean(buildProperties.get(RELEASE_PROPERTY_KEY));
+				if (!release)
+				{
+					str = buildProperties.get(BUILD_PROPERTY_KEY);
+					if (str != null)
+					{
+						if (buffer.length() > 0)
+							buffer.append(' ');
+						buffer.append(str);
+					}
 				}
 			}
+			else
+			{
+				long time = System.currentTimeMillis();
+				buffer.append('b');
+				buffer.append(CalendarTime.dateToString(time));
+				buffer.append('-');
+				buffer.append(CalendarTime.hoursMinsToString(time));
+			}
+			versionStr = buffer.toString();
 		}
-
-		return buffer.toString();
+		return versionStr;
 	}
 
 	//------------------------------------------------------------------
@@ -1600,6 +1610,7 @@ public class App
 ////////////////////////////////////////////////////////////////////////
 
 	private	ResourceProperties	buildProperties;
+	private	String				versionStr;
 	private	MainWindow			mainWindow;
 	private	boolean				hasGui;
 	private	boolean				titleShown;
